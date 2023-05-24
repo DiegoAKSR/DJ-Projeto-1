68c8f70df94ee71ad579df8c231bf6c4b909e7e4
Diego Nunes <diegonmt@gmail.com>
2023-05-24 20:00:58 -0300

    add

--- news/models.py
+++ news/models.py
@@ -21,7 +21,8 @@ class New(models.Model):
     cover = models.ImageField(
         upload_to='news/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,)
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
 
--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
 from news.models import New
 from util.news.factory import make_recipe
@@ -8,7 +9,6 @@ def home(request):
     news = New.objects.filter(is_published=True).order_by('-id')
     return render(request, 'news/pages/home.html',
                   context={'news': news,
-
                            })
 
 
@@ -16,14 +16,21 @@ def category(request, category_id):
     news = New.objects.filter(
         category__id=category_id, is_published=True
     ).order_by('-id')
+
+    if not news:
+        raise Http404("Not Found 😱")
+
     return render(request, 'news/pages/category.html',
                   context={'news': news,
+                           'title': f'{news.first().category.name} - Category'
 
                            })
 
 
 def news(request, id):
+    new = get_object_or_404(New, pk=id, is_published=True)
+
     return render(request, 'news/pages/news-view.html',
-                  context={'new': make_recipe(),
+                  context={'new': new,
                            'is_detail_page': True
                            })
