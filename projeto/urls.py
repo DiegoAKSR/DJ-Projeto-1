17dc8ee6fa349019110d2cafc018335bf199c3eb
DiegoAKSR <125274098+DiegoAKSR@users.noreply.github.com>
2023-09-24 11:11:06 -0300

    update static

--- projeto/urls.py
+++ projeto/urls.py
@@ -2,7 +2,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import include, path
 
 urlpatterns = [
